be6708840fb31aa9791074dbad738cdf9c08b373
Leonardo-Florencio <leonardo_florenciop@outlook.com>
2024-09-14 12:17:11 -0300

    utilização do método int() para reconhecimento da decisão dentro do if-elif

--- app.py
+++ app.py
@@ -5,5 +5,15 @@ print('2. Listar restaurantes')
 print('3. Ativar restaurante')
 print('4. Sair da aplicação\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+opcao_escolhida = int(input('Escolha uma opção: '))
 print(f'Você escolheu a opção {opcao_escolhida}')
+#opcao_escolhida = int(opcao_escolhida)
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print ('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print ('Ativar restaurante')
+else:
+    print('Encerrando programa')
