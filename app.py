c3e086af7de1fbbd815a7bfa9ff5a430ba634099
Leonardo-Florencio <leonardo_florenciop@outlook.com>
2024-09-14 12:24:37 -0300

    aplicação da lib "os" para limpeza do cmd, assim como a criação de função para sair da aplicação

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os #utilizado para chamar a biblioteca OS, que permite o uso do método os.system('cls), que limpa o cmd quando utilizado
+
 print('Sabor Express\n')
 
 print('1. Cadastrar restaurante')
@@ -9,6 +11,12 @@ opcao_escolhida = int(input('Escolha uma opção: '))
 print(f'Você escolheu a opção {opcao_escolhida}')
 #opcao_escolhida = int(opcao_escolhida)
 
+
+def finalizar_app():
+    os.system('cls')
+    print ('Encerrando o app\n')
+
+
 if opcao_escolhida == 1:
     print('Cadastrar restaurante')
 elif opcao_escolhida == 2:
@@ -16,4 +24,4 @@ elif opcao_escolhida == 2:
 elif opcao_escolhida == 3:
     print ('Ativar restaurante')
 else:
-    print('Encerrando programa')
+    finalizar_app()
