fa38bb3deb1bd7cb6d2cc3df6bd806f619e11df6
Leonardo-Florencio <leonardo_florenciop@outlook.com>
2024-09-14 11:38:42 -0300

    First docs

---
+++ app.py
@@ -0,0 +1,9 @@
+print('Sabor Express\n')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurantes')
+print('3. Ativar restaurante')
+print('4. Sair da aplicação\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {opcao_escolhida}')
---
+++ exercicios_complementares.py
@@ -0,0 +1,8 @@
+print ('Python na Escola de Programação da Alura\n')
+
+nome = input ('Digite o nome: ')
+idade = input ('Digite a idade: ')
+print(f'Meu nome é {nome} e tenho {idade} anos.\n')
+
+print('A\nL\nU\nR\nA\n')
+
