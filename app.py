28a36ce770c103917a363e4237a1bd25f097cbf8
Leonardo-Florencio <leonardo_florenciop@outlook.com>
2024-09-19 22:00:42 -0300

    criação do método de modificação de status, assim como refatoração para facilitar a visualização no console

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os #utilizado para chamar a biblioteca OS, que permite o uso do método os.system('cls), que limpa o cmd quando utilizado
 
-array_restaurantes = ['Pizza','Sushi']
+array_restaurantes = [{'nome':'Praça', 'categoria':'Japonesa', 'ativo':False},
+                      {'nome': 'Pizza Suprema', 'categoria':'Pizza', 'ativo':True},
+                      {'nome':'Cantina', 'categoria':'Italiana', 'ativo':False}] #utilização de dicionários, conhecidos em outras linguagens como "chave-valor", quase a mesma ideia de um objeto em JS.
 
 def exibir_nome_do_programa():
     print('Sabor Express\n')
@@ -8,7 +10,7 @@ def exibir_nome_do_programa():
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurantes')
-    print('3. Ativar restaurante')
+    print('3. Alternar estado do restaurante')
     print('4. Sair da aplicação\n')
 
 def finalizar_app():
@@ -24,22 +26,50 @@ def opcao_invalida():
     
 def exibir_subtitulos(texto):
     os.system('cls')
+    linha = '*' * (len(texto))
+    print(linha)
     print(texto)
+    print(linha)
     print()
 
 def cadastrar_novo_restaurante():
     exibir_subtitulos('Cadastro de novos restaurantes')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
-    array_restaurantes.append(nome_do_restaurante)
+    categoria_do_restaurante = input(f'Digite o nome da categoria do restaurante {nome_do_restaurante}: ')
+    
+    dados_do_restaurante = {'nome':nome_do_restaurante, 'categoria':categoria_do_restaurante, 'ativo':False}
+    array_restaurantes.append(dados_do_restaurante)
+    
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!\n')
     voltar_ao_menu_principal()
     
 def listar_restaurantes():
     exibir_subtitulos('Listando restaurantes')
     
+    print(f'{'Nome do restaurante'.ljust(22)} | {'Categoria'.ljust(20)} | {'Status'}')
     for restaurante in array_restaurantes:
-        print(f'.{restaurante}')
+        nome_restaurante = restaurante['nome']
+        categoria_restaurante = restaurante['categoria']
+        status_restaurante = 'Ativado' if restaurante['ativo'] else 'Desativado' #método ternário de teste, ao invés do bloco padrão - valor se verdadeiro, teste que deve ser verdadeiro e valor caso seja false apos o else
+        print(f'- {nome_restaurante.ljust(20)} | {categoria_restaurante.ljust(20)} | {status_restaurante}')
         
+    voltar_ao_menu_principal()
+
+def alternar_estado_restaurante():
+    exibir_subtitulos('Alternando estado do restaurante')
+    nome_restaurante = input('Digite o nome do restaurante que deseja alternar o estado: ')
+    restaurante_encontrado = False
+    
+    for restaurante in array_restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo'] #not é um método que inverte o valor, se for true, vai para false e vice-versa
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante  {nome_restaurante} foi desativado com sucesso'
+            print(mensagem)
+            
+    if not restaurante_encontrado:
+        print('O restaurante não foi encontrado')
+    
     voltar_ao_menu_principal()
 
 def escolher_opcao():
@@ -52,7 +82,7 @@ def escolher_opcao():
         elif opcao_escolhida == 2:
             listar_restaurantes()
         elif opcao_escolhida == 3:
-            print ('Ativar restaurante')
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4:
             finalizar_app()
         else:
